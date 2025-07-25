1151495ad48225690f71c1e8c2080ce93432ec1b
Mohammed Shamil <kmshamil123@gmail.com>
2025-07-25 18:19:48 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,6 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    {{-- @vite('resources/css/app.css') --}}
-
     @yield('style')
 
 
