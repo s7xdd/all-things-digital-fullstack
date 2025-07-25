26b63714e46f6b7da0fbf06ea16f56dccf30c180
Mohammed Shamil <kmshamil123@gmail.com>
2025-07-25 18:23:35 +0400

    add

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,8 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                // 'resources/css/app.css',
+                // 'resources/js/app.js',
+                'public/build/assets/app-ab0e78c4-dd6fdc47.js',
+                'public/build/assets/app-bfd32cbf-bfd32cbf.css',
+                'public/build/assets/app-f055accc-f055accc.css',
             ],
             refresh: true,
         }),
