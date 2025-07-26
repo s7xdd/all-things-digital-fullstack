7a7bfd7e55657cbf3e78d5af360dcef4ed431645
Mohammed Shamil <kmshamil123@gmail.com>
2025-07-26 17:37:55 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="icon" type="image/png" href="{{ get_setting('site_icon') }}">
+    <link rel="icon" type="image/png" href="{{ uploaded_asset(get_setting('site_icon')) }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
